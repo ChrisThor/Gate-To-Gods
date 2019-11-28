4969c1d55934608a7895bdc8350a99ab744dbc4a
Krister Wolfhard <krister.wolfhard@it-designers.de>
2019-11-28 15:48:50 +0100

    Change the structure of colours.py
    colours.py now uses a dictionary instead of individual variables to make
    sure it can support user defined colours later.

--- colours.py
+++ colours.py
@@ -1,40 +1,28 @@
 class Colours:
     def __init__(self):
-        self.clear_screen = "\033[2J"
-        self.jump_to_top = "\033[H"
-        self.grey = "\033[38;2;100;100;100m"
-        self.red = "\033[31m"
-        self.green = "\033[32m"
-        self.yellow = "\033[33m"
-        self.blue = "\033[34m"
-        self.magenta = "\033[35m"
-        self.cyan = "\033[36m"
-        self.white = "\033[37m"
-        self.default = "\033[0m"
+        self.colours = dict()
+        self.colours["clear_screen"] = "\033[2J"
+        self.colours["jump_to_top"] = "\033[H"
+        self.colours["grey"] = "\033[38;2;100;100;100m"
+        self.colours["red"] = "\033[31m"
+        self.colours["green"] = "\033[32m"
+        self.colours["yellow"] = "\033[33m"
+        self.colours["blue"] = "\033[34m"
+        self.colours["magenta"] = "\033[35m"
+        self.colours["cyan"] = "\033[36m"
+        self.colours["white"] = "\033[37m"
+        self.colours["default"] = "\033[0m"
 
     def get_colour(self, colour: str):
-        if colour == "grey":
-            return self.grey
-        elif colour == "red":
-            return self.red
-        elif colour == "green":
-            return self.green
-        elif colour == "blue":
-            return self.blue
-        elif colour == "yellow":
-            return self.yellow
-        elif colour == "cyan":
-            return self.cyan
-        elif colour == "white":
-            return self.white
-        else:
-            return self.default
+        if colour in self.colours:
+            return self.colours[colour]
+        return self.colours["default"]
 
     def reset(self):
-        return self.default
+        return self.get_colour("default")
 
     def clear(self):
-        return self.clear_screen
+        return self.get_colour("clear_screen")
 
     def jump_up(self):
-        return self.jump_to_top
+        return self.get_colour("jump_to_top")
