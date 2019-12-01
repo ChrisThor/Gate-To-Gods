1dd1d6e187496e495ec2421f6dd211181365d6f8
ChrisThor <krister.wolfhard@it-designers.de>
2019-12-01 20:17:23 +0100

    Add the screen size to "config.txt"
    Also changes the place where the file is read from language.py to
    main.py

--- language.py
+++ language.py
@@ -1,27 +1,11 @@
 class LanguageManagement:
-    def __init__(self):
+    def __init__(self, language_file: str):
         self.texts = {}
         self.undefined = "You tried to access a text or message that is not defined in your current language file."
-        self.read_language_file(self.get_language_file())
+        self.read_language_file(language_file)
 
-    def get_language_file(self):
-        config = None
-        try:
-            config = open("data/config.txt", "r")
-        except FileNotFoundError:
-            print("\"config.txt\" could not be found.")
-            exit(-1)
-        lines = config.readlines()
-        config.close()
-        for line in lines:
-            if "language_file" in line:
-                try:
-                    return line.split("=")[1].replace("\n", "")
-                except IndexError:
-                    print("\"config.txt\" is incorrect.")
-                    exit(-1)
-
-    def read_language_file(self, filename):
+    def read_language_file(self, filename: str):
+        self.texts = {}
         language_file = None
         try:
             language_file = open("data/" + filename, "r")
