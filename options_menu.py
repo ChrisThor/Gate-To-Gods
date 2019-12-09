c57e98c12a3004b1191d3bbf4daec24162e6d786
Krister Wolfhard <krister.wolfhard@it-designers.de>
2019-12-09 14:36:22 +0100

    Change option "Screen Dimensions"

--- options_menu.py
+++ options_menu.py
@@ -1,4 +1,5 @@
 from box import Box
+import keyboard_input
 import os
 
 
@@ -11,9 +12,10 @@ class OptionsMenu:
         self.change_language_box = Box(content=gtg.language.texts.get("option_new_file"),
                                box_width=len(gtg.language.texts.get("option_new_file")),
                                options=os.listdir(os.getcwd() + "/data/lang"))
+        self.change_screen_box = Box(content=gtg.language.texts.get("option_new_screen"),
+                                     box_width=27)
 
     def enter_menu(self, gtg):
-        jump_position = "\033[" + str(gtg.scr.len_y + 8) + ";0H"
         selected_option = ""
         self.display_box.active_option = 0
         while selected_option != gtg.language.texts.get("option_go_back"):
@@ -22,22 +24,55 @@ class OptionsMenu:
             if selected_option == gtg.language.texts.get("option_change_language"):
                 self.change_language(gtg)
             elif selected_option == gtg.language.texts.get("option_change_screen"):
-                self.change_screen_dimensions(gtg, jump_position)
+                self.change_screen_dimensions(gtg)
         configurations = open("data/config.txt", "w")
         for key in gtg.configurations:
             configurations.write(str(key) + "=" + str(gtg.configurations[key]) + "\n")
         configurations.close()
 
-    def change_screen_dimensions(self, gtg, jump_position):
-        Box(content=gtg.language.texts.get("option_new_screen"),
-            box_width=len(gtg.language.texts.get("option_new_screen")),
-            colour=gtg.colours.get_random_colour(gtg.rng, True)) \
-            .print_box(gtg.scr)
-        new_dimensions = input(jump_position + "> ")
-        buffer = new_dimensions.split(":")
-        gtg.scr.change_size(int(buffer[0]), int(buffer[1]))
-        gtg.configurations["screen_height"] = buffer[0]
-        gtg.configurations["screen_width"] = buffer[1]
+    def change_screen_dimensions(self, gtg):
+        key = ""
+        len_y = gtg.scr.len_y
+        len_x = gtg.scr.len_x
+        original_y = len_y
+        original_x = len_x
+        lower_x_limit = 33
+        lower_y_limit = 11
+        self.change_screen_box.colour = gtg.colours.get_random_colour(gtg.rng, True)
+        while key != "KEY_ENTER" and key != "KEY_ESCAPE":
+            print(f"\033[H{gtg.colours.clear()}{gtg.language.texts.get('screen_height')}: {len_y} "
+                  f"{gtg.language.texts.get('screen_width')}: {len_x}")
+            self.print_screen_size_preview(gtg.scr.len_y, gtg.scr.len_x)
+            self.change_screen_box.print_box(gtg.scr)
+            key = keyboard_input.read_keyboard()
+
+            if key == "KEY_ARROW_UP":
+                len_y -= 1
+            elif key == "KEY_ARROW_DOWN":
+                len_y += 1
+            elif key == "KEY_ARROW_LEFT":
+                len_x -= 1
+            elif key == "KEY_ARROW_RIGHT":
+                len_x += 1
+            if len_y < lower_y_limit:
+                len_y = lower_y_limit
+            if len_x < lower_x_limit:
+                len_x = lower_x_limit
+
+            gtg.scr.change_size(len_y, len_x)
+        print(gtg.colours.clear())
+        if key == "KEY_ESCAPE":
+            gtg.scr.change_size(original_y, original_x)
+        else:
+            gtg.configurations["screen_height"] = len_y
+            gtg.configurations["screen_width"] = len_x
+
+    def print_screen_size_preview(self, len_y: int, len_x: int):
+        len_y -= 1
+        y_offset = 3
+        symbol = "*"
+        print(f"\033[{y_offset};0H{symbol}\033[{y_offset};{len_x}H{symbol}\033[{y_offset + len_y};0H{symbol}"
+              f"\033[{y_offset + len_y};{len_x}H{symbol}")
 
     def change_language(self, gtg):
         self.change_language_box.colour = gtg.colours.get_random_colour(gtg.rng, True)
